72ba1558182a42229cfdeea47ae7abb734c5d842
devken4u <devken4u@gmail.com>
2024-10-06 19:50:08 +0800

    refactor(fruit list comonent): Updated with services feature

--- src/app/fruit-list/fruit-list.component.ts
+++ src/app/fruit-list/fruit-list.component.ts
@@ -1,20 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FruitService } from '../services/fruit.service';
 import { Fruit } from '../types';
 
 @Component({
-  selector: 'app-fruit-list',
+  selector: 'app-fruit',
   templateUrl: './fruit-list.component.html',
   styleUrl: './fruit-list.component.css',
 })
-export class FruitListComponent {
+export class FruitListComponent implements OnInit {
   fruitList: Fruit[] = [];
-
   name: string = '';
 
-  add() {
-    this.fruitList.push({
-      name: this.name,
-    });
-    this.name = '';
+  constructor(private fruitService: FruitService) {}
+
+  ngOnInit(): void {
+    this.fruitList = this.fruitService.getFruitList();
+  }
+
+  add(): void {
+    if (this.name) {
+      this.fruitService.addFruit({
+        name: this.name,
+      });
+
+      this.name = '';
+    }
+  }
+
+  deleteFruit(index: number): void {
+    this.fruitService.deleteFruit(index);
+  }
+
+  editField(index: number, fieldName: keyof Fruit, currentValue: string) {
+    const value = prompt(`Edit ${fieldName}`, currentValue);
+    if (value !== null) {
+      const tempFruit = { ...this.fruitList[index], [fieldName]: value };
+      this.update(index, tempFruit);
+    }
+  }
+
+  update(index: number, fruit: Fruit): void {
+    this.fruitService.editFruit(index, fruit);
   }
 }
